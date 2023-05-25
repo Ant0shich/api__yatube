a20650a72caa70ae5a8e95e20ed99eb382c4b0d2
Антон Бородин <borodin6746@mail.ru>
2023-05-25 07:59:18 +0300

    new

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -17,11 +17,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'django_filters',
-    'djoser',
     'api',
     'posts',
+    'django_filters',
+    'djoser',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
